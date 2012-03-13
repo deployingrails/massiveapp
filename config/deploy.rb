3d4b443f5d31f133f77b3f7715049102a570d8d2
Tom Copeland <tom.copeland@livingsocial.com>
2012-03-12 22:25:04 -0400

    Fixing path to Vagrant private key

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,25 @@
-# START:require_and_application
 require 'bundler/capistrano'
 set :application, "massiveapp"
-# END:require_and_application
-# START:require_whenever
+
 set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
-# END:require_whenever
-# START:scm
+
 set :scm, :git
-set :repository, "."
-# END:scm
-# START:host
+set :repository, "git://github.com/deployingrails/massiveapp.git"
+
 server "localhost", :app, :db, :primary => true
-# END:host
-# START:ssh
+
 ssh_options[:port] = 2222
-ssh_options[:keys] = "/Library/Ruby/Gems/1.8/gems/vagrant-0.8.10/keys/vagrant"
-# END:ssh
-# START:user
+ssh_options[:keys] = "~/.vagrant.d/insecure_private_key"
+
 set :user, "vagrant"
 set :group, "vagrant"
 set :deploy_to, "/var/massiveapp"
 set :use_sudo, false
-# END:user
-# START:copy_strategy
+
 set :deploy_via, :copy
 set :copy_strategy, :export
-# END:copy_strategy
-# START:passenger
+
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
@@ -36,7 +28,6 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
-# END:passenger
 
 after "deploy:symlink", "deploy:copy_in_database_yml"
 
@@ -50,6 +41,7 @@ desc "Download the production log file"
 task :get_dir do |t|
   get "#{current_path}/log/", "#{Time.now}.log"
 end
+
 desc "Download the production log file"
 task :get_log do |t|
   get "#{current_path}/log/production.log", "#{Time.now.strftime("%Y%m%d%H%M")}.production.log"
