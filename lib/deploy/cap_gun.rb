8697999c3c55fcce00974127b4877b5d0ae8f587
Tom Copeland <tom.copeland@livingsocial.com>
2012-03-29 09:49:10 -0400

    Temporarily (?) removing cap_gun

--- lib/deploy/cap_gun.rb
+++ lib/deploy/cap_gun.rb
@@ -1,25 +1,25 @@
-# START:require
-require 'cap_gun'
-# END:require
-
-# START:smtp
-set :cap_gun_action_mailer_config, {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :user_name => "[YOUR_USERNAME]@gmail.com",
-  :password => "[YOUR_PASSWORD]",
-  :authentication => :plain
-}
-# END:smtp
-
-# START:envelope
-set :cap_gun_email_envelope, {
-  :from => "deploy@massiveapp.com",
-  :recipients => %w{me@massiveapp.com coworker@massiveapp.com},
-  :prefix => "massiveapp deployed: "
-}
-# END:envelope
-
-# START:hook
-after "deploy:restart", "cap_gun:email"
-# END:hook
+# # START:require
+# require 'cap_gun'
+# # END:require
+# 
+# # START:smtp
+# set :cap_gun_action_mailer_config, {
+#   :address => "smtp.gmail.com",
+#   :port => 587,
+#   :user_name => "[YOUR_USERNAME]@gmail.com",
+#   :password => "[YOUR_PASSWORD]",
+#   :authentication => :plain
+# }
+# # END:smtp
+# 
+# # START:envelope
+# set :cap_gun_email_envelope, {
+#   :from => "deploy@massiveapp.com",
+#   :recipients => %w{me@massiveapp.com coworker@massiveapp.com},
+#   :prefix => "massiveapp deployed: "
+# }
+# # END:envelope
+# 
+# # START:hook
+# after "deploy:restart", "cap_gun:email"
+# # END:hook
