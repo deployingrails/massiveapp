0afa211ab84cc2bce52539143a67f6592a658025
Tom Copeland <tom.copeland@livingsocial.com>
2012-04-14 13:43:44 -0400

    Removing capistrano

--- Gemfile
+++ Gemfile
@@ -13,8 +13,3 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
-
-group :development do
-  gem 'cap_gun'
-  gem 'capistrano', '~> 2.11.2'
-end
--- lib/deploy/db.rb
+++
@@ -1,6 +0,0 @@
-namespace :deploy do
-  desc "Copy the database.yml file into current/"
-  task :copy_in_database_yml do
-    run "cp #{shared_path}/config/database.yml #{current_path}/config/"
-  end
-end
